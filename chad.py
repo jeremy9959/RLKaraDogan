9e751f5bc45c4a9fa4fd722690e69eec136ef1dc
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2025-01-23 15:29:14 -0500

    updated

--- chad.py
+++
@@ -1,5 +0,0 @@
-from tqdm import tqdm
-
-for i in tqdm(range(10)):
-    for j in tqdm(range(10000000)):
-        pass
--- q_learning_agent.py
+++ q_learning_agent.py
@@ -1,11 +1,22 @@
 import numpy as np
 import random
 
+
 class QLearningAgent:
-    def __init__(self, state_size, action_size, learning_rate=0.3, learning_rate_decay_parameter=.999, discount_factor=0.99, exploration_rate=1.0, exploration_decay_parameter=0.95, min_exploration_rate=0.0):
+    def __init__(
+        self,
+        state_size,
+        action_size,
+        learning_rate=0.3,
+        learning_rate_decay_parameter=0.999,
+        discount_factor=0.99,
+        exploration_rate=1.0,
+        exploration_decay_parameter=0.95,
+        min_exploration_rate=0.0,
+    ):
         """
         Initialize the Q-learning agent.
-        
+
         :param state_size: Size of the state space
         :param action_size: Size of the action space
         :param learning_rate: Learning rate for Q-learning
@@ -22,13 +33,12 @@ class QLearningAgent:
         self.exploration_rate = exploration_rate
         self.exploration_decay_parameter = exploration_decay_parameter
         self.min_exploration_rate = min_exploration_rate
-        self.q_table = np.zeros((state_size, action_size))
-        self.q_table.fill(10000)
+        self.q_table = np.random.rand(state_size, action_size)
 
     def choose_action(self, state):
         """
         Choose an action based on the current state using epsilon-greedy policy.
-        
+
         :param state: Current state
         :return: Action to take
         """
@@ -42,7 +52,7 @@ class QLearningAgent:
     def update_q_table(self, state, action, reward, next_state):
         """
         Update the Q-table based on the action taken and the reward received.
-        
+
         :param state: Previous state
         :param action: Action taken
         :param reward: Reward received
@@ -51,25 +61,29 @@ class QLearningAgent:
         max_value = np.max(self.q_table[next_state])
         td_target = reward + self.discount_factor * max_value
         td_error = td_target - self.q_table[state][action]
-        self.q_table[state][action] += (self.learning_rate * td_error)
+        self.q_table[state][action] += self.learning_rate * td_error
 
-
-    def decay(self,param, delta,time):
+    def decay(self, param, delta, time):
         """
         Decays the parameter according to the Kara-Dogan rule
         """
-        y=time**2/(delta+time)
-        new_param = param/(1+y)
+        y = time**2 / (delta + time)
+        new_param = param / (1 + y)
         return new_param
-    
+
     def decay_exploration_rate(self, time):
         """
         Decay the exploration rate.
         """
-        self.exploration_rate = max(self.min_exploration_rate, self.decay(self.exploration_rate,self.exploration_decay_parameter,time))
+        self.exploration_rate = max(
+            self.min_exploration_rate,
+            self.decay(self.exploration_rate, self.exploration_decay_parameter, time),
+        )
 
     def decay_learning_rate(self, time):
         """
         Decay the learning rate.
         """
-        self.learning_rate = self.decay(self.learning_rate,self.learning_rate_decay_parameter,time)
+        self.learning_rate = self.decay(
+            self.learning_rate, self.learning_rate_decay_parameter, time
+        )
